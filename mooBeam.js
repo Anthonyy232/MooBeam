1e83d6e1f4cce631a49954409cf313b9f900715f
Anthonyy232 <Anthonyyla232@gmail.com>
2024-02-29 18:13:29 -0800

    Added primitive movements in all direction, floor, sky

--- mooBeam.js
+++ mooBeam.js
@@ -6,7 +6,7 @@ const {
 } = tiny;
 
 const {
-    Textured_Phong, Fake_Bump_Map, Cube
+    Textured_Phong, Fake_Bump_Map, Square
 } = defs
 
 class player {
@@ -20,45 +20,70 @@ class player {
 export class MooBeam extends Scene {
     constructor() {
         super();
-        this.player = new player(0, 0 , 0);
-        this.ufo_state = Mat4.identity();
         this.shapes = {
-            object: new defs.Subdivision_Sphere(1),
+            object: new defs.Subdivision_Sphere(4),
             ufo: new Shape_From_File("assets/Ufo.obj"),
-            sky: new defs.Subdivision_Sphere(4),
+            sky: new defs.Subdivision_Sphere(5),
+            floor: new Square()
         };
+        this.start_y = 20;
+        this.movement_speed = 10;
+
+
+        this.sky_state = Mat4.identity().times(Mat4.scale(200, 200, 200));
+        this.floor_state = Mat4.identity().times(Mat4.scale(200, 200, 200)).times(Mat4.rotation(Math.PI / 2, 1, 0, 0));
+        this.player = new player(0, this.start_y , 0);
+        this.ufo_state = Mat4.identity();
 
         // *** Materials
         this.materials = {
-            object_material: new Material(new defs.Phong_Shader(),
-                {ambient: 0.5, diffusivity: 0.3, color: hex_color('#80FFFF'), specularity: 1}),
             ufo_material: new Material(new Fake_Bump_Map(1), {
-                color: hex_color("#000000"), ambient: 0.5, diffusivity: 0.5, specularity: 0.5,
+                color: hex_color("#000000"), ambient: 0.5, diffusivity: 0.5, specularity: 1,
                 texture: new Texture("assets/stars.png")
             }),
             skybox: new Material(new Textured_Phong, {
                 color: hex_color("#000000"), ambient: 1, diffusivity: 0, specularity: 0,
                 texture: new Texture("assets/skybox.png")
             }),
-
+            floor_material: new Material(new Fake_Bump_Map, {
+                color: hex_color("#000000"), ambient: 0.5, diffusivity: 0, specularity: 1,
+                texture: new Texture("assets/floor.jpg")
+            })
         }
-
-        this.initial_camera_location = Mat4.look_at(vec3(0, 10, 20), vec3(0, 0, 0), vec3(0, 1, 0));
+        this.initial_camera_location = Mat4.look_at(vec3(0, 10 + this.start_y, 20), vec3(0, this.start_y, 0), vec3(0, 1 + this.start_y, 0));
     }
 
     make_control_panel() {
-        this.key_triggered_button("View origin", ["Control", "0"], () => this.attached = () => null);
+        this.key_triggered_button("Isometric View", ["Control", "0"], () => this.attached = () => null);
         this.new_line();
-        this.key_triggered_button("Attach to object", ["Control", "1"], () => this.attached = () => this.object);
+        this.key_triggered_button("Behind View", ["Control", "1"], () => this.attached = () => this.object);
         this.key_triggered_button("Move forward", ["w"], this.move_forward);
+        this.key_triggered_button("Move backward", ["s"], this.move_backward);
+        this.key_triggered_button("Move left", ["a"], this.move_left);
+        this.key_triggered_button("Move right", ["d"], this.move_right);
     }
 
     move_forward() {
         //smooth motion but not smooth enough, consider transitioning to position, vel, accel style
         //need to take care of release key if doing velocity+accel
-        let target = {z: this.player.z + 1};
-        let t = 0.05;
+        let target = {z: this.player.z - this.movement_speed};
+        let t = 0.01;
         this.player.z += (target.z - this.player.z) * t;
+    }
+    move_backward() {
+        let target = {z: this.player.z + this.movement_speed};
+        let t = 0.01;
+        this.player.z += (target.z - this.player.z) * t;
+    }
+    move_left() {
+        let target = {x: this.player.x - this.movement_speed};
+        let t = 0.01;
+        this.player.x += (target.x - this.player.x) * t;
+    }
+    move_right() {
+        let target = {x: this.player.x + this.movement_speed};
+        let t = 0.01;
+        this.player.x += (target.x - this.player.x) * t;
     }
 
     display(context, program_state) {
@@ -81,24 +106,26 @@ export class MooBeam extends Scene {
                 .times(Mat4.translation(0,5,15))
                 .times(Mat4.rotation(-Math.PI / 8, 1, 0, 0 ))
             )
+            let angle = Math.atan(1 / Math.sqrt(2));
             let isometric = Mat4.inverse(Mat4.identity()
-                //todo
-            )
+                .times(Mat4.rotation(angle, 1, 0, 0))
+                .times(Mat4.rotation(Math.PI / 4, 0, 1, 0))
+                .times(Mat4.translation(-this.player.x, -this.player.y, -this.player.z))
+            );
             this.object = this.ufo_state;
-            let desired = this.attached && this.attached() != null ? third_person : this.initial_camera_location
+            let desired = this.attached && this.attached() != null ? third_person : this.initial_camera_location // <--set as initial until isometric works
             program_state.set_camera(desired.map((x,i) => Vector.from(program_state.camera_inverse[i]).mix(x, 0.1)));
+            //^figure out how to do quaternion blending so when pressing the keys there isn't a slight camera give
         }
 
-
         // The parameters of the Light are: position, color, size
-        const light_position = vec4(0, 5, 2, 0);
+        let light_position = vec4(this.player.x, this.player.y, this.player.z, 0);
         program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
 
-        this.shapes.ufo.draw(context, program_state, this.ufo_state, this.materials.ufo_material);
 
-        let model_transform_sky = Mat4.identity();
-        model_transform_sky = model_transform_sky.times(Mat4.scale(100, 100, 100));
-        this.shapes.sky.draw(context, program_state, model_transform_sky, this.materials.skybox);
+        this.shapes.ufo.draw(context, program_state, this.ufo_state, this.materials.ufo_material);
+        this.shapes.sky.draw(context, program_state, this.sky_state, this.materials.skybox);
+        this.shapes.floor.draw(context, program_state, this.floor_state, this.materials.floor_material);
     }
 }
 
