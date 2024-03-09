afcd8ddad4b10cfc9bc2a1ae0fd8b4f7e36f069c
Anthonyy232 <Anthonyyla232@gmail.com>
2024-03-08 17:28:43 -0800

    Fixed typos that caused program not to function

--- mooBeam.js
+++ mooBeam.js
@@ -45,15 +45,15 @@ export class MooBeam extends Scene {
         this.shapes = {
             object: new defs.Subdivision_Sphere(4),
             ufo: new Shape_From_File("assets/Ufo.obj"),
-            cow: new Shape_From_File("assets/cow.obj"),
+            //cow: new Shape_From_File("assets/cow.obj"),
             sky: new defs.Subdivision_Sphere(5),
             floor: new Square(),
             skyscraper: new Cube(),
             beam: new defs.Rounded_Closed_Cone(4, 10, [[0, 2], [0, 1]]),
         };
 
-        this.starting_location = new player(0, 20, 0)
-        this.cow_start = vec3(10, 2, 0);
+        this.starting_location = new Player(0, 20, 0)
+        //this.cow_start = vec3(10, 2, 0);
         this.movement_speed = 100;
         this.begin_game = false;
         this.end_game = false;
@@ -78,15 +78,13 @@ export class MooBeam extends Scene {
         this.world_size = 200;
         this.sky_state = Mat4.identity().times(Mat4.scale(this.world_size, this.world_size, this.world_size));
         this.floor_state = Mat4.identity().times(Mat4.scale(this.world_size, this.world_size, this.world_size)).times(Mat4.rotation(Math.PI / 2, 1, 0, 0));
-        this.player = new Player(0, this.starting_location.y , 0);
+        this.Player = new Player(0, this.starting_location.y , 0);
         this.ufo_state = Mat4.identity();
         this.skyscraper_height = 25;
         this.skyscraper_size = 10;
         this.skyscraper_state = Mat4.identity()
             .times(Mat4.scale(this.skyscraper_size, this.skyscraper_height, this.skyscraper_size))
         this.skyscraper_count = 1;
-
-
 
         // *** Materials
         this.materials = {
@@ -97,10 +95,11 @@ export class MooBeam extends Scene {
                 color: hex_color("#000000"), ambient: 0.2, diffusivity: 0.5, specularity: 1,
                 texture: new Texture("assets/ufo.jpg")
             }),
+            /*
             cow_material: new Material(new defs.Fake_Bump_Map(1), {
                 color: hex_color("#000000"), ambient: 0.2, diffusivity: 0.5, specularity: 1,
                 texture: new Texture("assets/ufo.jpg")
-            }),
+            }), */
             skybox: new Material(new defs.Fake_Bump_Map(1), {
                 color: hex_color("#000000"), ambient: 1, diffusivity: 0, specularity: 0,
                 texture: new Texture("assets/skybox.png")
@@ -115,7 +114,6 @@ export class MooBeam extends Scene {
             })
         }
         this.initial_camera_location = Mat4.look_at(vec3(0, 10 + this.starting_location.y, 20), vec3(0, this.starting_location.y, 0), vec3(0, 1 + this.starting_location.y, 0));
-
 
         // Generate random skyscrapers
         this.skyscrapers_states = this.generateSkyscrapers(this.skyscraper_state);
@@ -146,22 +144,22 @@ export class MooBeam extends Scene {
         this.key_triggered_button("Behind View", ["Control", "1"], () => this.attached = () => this.object);
 
         this.key_triggered_button("Move forward", ["i"], this.move_forward, "#6E6460", () => {
-            this.player.velocity.z = 0;
+            this.Player.velocity.z = 0;
         });
         this.key_triggered_button("Move backward", ["k"], this.move_backward, "#6E6460", () => {
-            this.player.velocity.z = 0;
+            this.Player.velocity.z = 0;
         });
         this.key_triggered_button("Move left", ["j"], this.move_left, "#6E6460", () => {
-            this.player.velocity.x = 0;
+            this.Player.velocity.x = 0;
         });
         this.key_triggered_button("Move right", ["l"], this.move_right, "#6E6460", () => {
-            this.player.velocity.x = 0;
+            this.Player.velocity.x = 0;
         });
         this.key_triggered_button("Reset game", ["r"], this.reset);
         this.key_triggered_button("Beam cows", ["b"], () => this.animated = "start");
         this.key_triggered_button("Log", ["c"], () => {
-            let distance = Math.sqrt((0 - this.player.x)**2 + (-10 - this.player.z)**2);
-            console.log("Player: " + this.player.x + " " + this.player.y + " " + this.player.z + " ")
+            let distance = Math.sqrt((0 - this.Player.x)**2 + (-10 - this.Player.z)**2);
+            console.log("Player: " + this.Player.x + " " + this.Player.y + " " + this.Player.z + " ")
             console.log("Distance: " + distance)
         });
     }
@@ -172,39 +170,40 @@ export class MooBeam extends Scene {
         this.end_game = false;
         this.score = 0;
         this.time = 90;
-        this.player = new Player(0, this.starting_location.y , 0);
+        this.Player = new Player(0, this.starting_location.y , 0);
         this.ufo_state = Mat4.identity();
-        this.cow_state = Mat4.identity().times(Mat4.translation(this.cow_start[0], this.cow_start[1], this.cow_start[2]));
+        //this.cow_state = Mat4.identity().times(Mat4.translation(this.cow_start[0], this.cow_start[1], this.cow_start[2]));
         this.skyscrapers_states = this.generateSkyscrapers(this.skyscraper_state);
-        this.player.velocity = {x: 30, y: 30, z: 30};
+        this.Player.velocity = {x: 30, y: 30, z: 30};
     }
 
     move_forward() {
         this.begin_game = true;
-        this.player.velocity.z -= 6*this.player.acceleration.z;
-        if (Math.abs(this.player.velocity.z) > this.max_speed) {
-            this.player.velocity.z = -this.max_speed
+        this.Player.velocity.z -= 6*this.Player.acceleration.z;
+        if (Math.abs(this.Player.velocity.z) > this.max_speed) {
+            this.Player.velocity.z = -this.max_speed
         }
-        this.player.z += this.player.velocity.z;
+        this.Player.z += this.Player.velocity.z;
     }
 
     move_backward() {
         this.begin_game = true;
-        this.player.velocity.z += 3*this.player.acceleration.z;
-        if (Math.abs(this.player.velocity.z) > this.max_speed) {
-            this.player.velocity.z = this.max_speed
+        this.Player.velocity.z += 3*this.Player.acceleration.z;
+        if (Math.abs(this.Player.velocity.z) > this.max_speed) {
+            this.Player.velocity.z = this.max_speed
         }
-        this.player.z += this.player.velocity.z;
+        this.Player.z += this.Player.velocity.z;
     }
 
     move_left() {
         this.begin_game = true;
-        this.player.velocity.x -= 3*this.player.acceleration.x;
-        if (Math.abs(this.player.velocity.x) > this.max_speed) {
-            this.player.velocity.x = -this.max_speed
+        this.Player.velocity.x -= 3*this.Player.acceleration.x;
+        if (Math.abs(this.Player.velocity.x) > this.max_speed) {
+            this.Player.velocity.x = -this.max_speed
         }
-        this.player.x += this.player.velocity.x;
+        this.Player.x += this.Player.velocity.x;
     }
+    /*
     animate_cow(start_time, program_time) {
         let working_time = program_time - start_time;
         this.stop_time;
@@ -215,21 +214,20 @@ export class MooBeam extends Scene {
             this.cow_state = this.cow_state.times(Mat4.translation(0, this.stop_time * 0.01, 0));
         }
     }
+     */
 
     move_right() {
         this.begin_game = true;
-        this.player.velocity.x += 3*this.player.acceleration.x;
-        if (Math.abs(this.player.velocity.x) > this.max_speed) {
-            this.player.velocity.x = this.max_speed
+        this.Player.velocity.x += 3*this.Player.acceleration.x;
+        if (Math.abs(this.Player.velocity.x) > this.max_speed) {
+            this.Player.velocity.x = this.max_speed
         }
-        this.player.x += this.player.velocity.x;
+        this.Player.x += this.Player.velocity.x;
     }
     display(context, program_state) {
         // Refresh the score and timer HTML elements
         score_html.innerHTML = this.score.toString()
         time_html.innerHTML = format_time(this.time)
-
-
 
         // Display bottom control panel
         if (!context.scratchpad.controls) {
@@ -245,38 +243,40 @@ export class MooBeam extends Scene {
             const time = program_state.animation_time / 1000
 
             this.ufo_state = Mat4.identity()
-                .times(Mat4.translation(this.player.x, this.player.y, this.player.z))
+                .times(Mat4.translation(this.Player.x, this.Player.y, this.Player.z))
                 //hover animation
                 .times(Mat4.translation(0, 0.3*Math.sin(time*2), 0))
                 .times(Mat4.rotation(time / 2.5, 0 , 1, 0))
 
-            if (this.animated == "false") {
+            /*
+            if (this.animated === "false") {
                 this.local_time = program_state.animation_time;
             }
-            else if (this.animated == "start") {
+            else if (this.animated === "start") {
                 this.animated = "on";
             }
             else {
             }
+             */
 
+            /*
             this.cow_state = Mat4.identity()
                 .times(Mat4.translation(this.cow_start[0], this.cow_start[1], this.cow_start[2]))
                 //.times(Mat4.translation(10, 0,0))
 
-            if ( this.player.y < this.cow_start[1] + 20 &&
-                this.player.x < this.cow_start[0] + 3 && this.player.x > this.cow_start[0] - 3 &&
-                this.player.z < this.cow_start[2] + 3 && this.player.z > this.cow_start[2] - 3) {
+            if ( this.Player.y < this.cow_start[1] + 20 &&
+                this.Player.x < this.cow_start[0] + 3 && this.Player.x > this.cow_start[0] - 3 &&
+                this.Player.z < this.cow_start[2] + 3 && this.Player.z > this.cow_start[2] - 3) {
 
                 this.animated = "start";
             }
-            console.log(this.player);
-            console.log(this.cow_start);
+             */
 
-            this.animate_cow(this.local_time, program_state.animation_time);
+            //this.animate_cow(this.local_time, program_state.animation_time);
 
             if (true) { // For testing purposes set to false so the camera can fly around
                 let third_person = Mat4.inverse(Mat4.identity()
-                    .times(Mat4.translation(this.player.x, this.player.y, this.player.z))
+                    .times(Mat4.translation(this.Player.x, this.Player.y, this.Player.z))
                     .times(Mat4.translation(0,5,13))
                     .times(Mat4.rotation(-Math.PI / 8, 1, 0, 0 ))
                 )
@@ -284,7 +284,7 @@ export class MooBeam extends Scene {
                 let isometric = Mat4.inverse(Mat4.identity()
                     .times(Mat4.rotation(angle, 1, 0, 0))
                     .times(Mat4.rotation(Math.PI / 4, 0, 1, 0))
-                    .times(Mat4.translation(-this.player.x, -this.player.y, -this.player.z))
+                    .times(Mat4.translation(-this.Player.x, -this.Player.y, -this.Player.z))
                 );
                 this.object = this.ufo_state;
                 let desired = this.attached && this.attached() != null ? third_person : this.initial_camera_location // <--set as initial until isometric works
@@ -293,10 +293,9 @@ export class MooBeam extends Scene {
 
             // The parameters of the Light are: position, color, size
             program_state.lights = [new Light(
-                Mat4.rotation(time / 300, this.player.x, this.player.y, this.player.z).times(vec4(3, 2, 10, 1)), color(1, .7, .7, 1), 10000)];
+                Mat4.rotation(time / 300, this.Player.x, this.Player.y, this.Player.z).times(vec4(3, 2, 10, 1)), color(1, .7, .7, 1), 10000)];
 
             this.shapes.ufo.draw(context, program_state, this.ufo_state, this.materials.ufo_material);
-            this.shapes.cow.draw(context, program_state, this.cow_state, this.materials.cow_material);
             this.shapes.sky.draw(context, program_state, this.sky_state, this.materials.skybox);
             this.shapes.floor.draw(context, program_state, this.floor_state, this.materials.floor_material);
 
@@ -305,10 +304,12 @@ export class MooBeam extends Scene {
                 .times(Mat4.translation(0, 10, 0)); //translate it vertically so the base is at the floor height
             this.shapes.beam.draw(context, program_state, beam_state, this.materials.regular); */
 
+            //this.shapes.cow.draw(context, program_state, this.cow_state, this.materials.cow_material);
+
             for(let i = 0; i < this.skyscrapers_states.length; i++) {
                 this.shapes.skyscraper.draw(context, program_state, this.skyscrapers_states[i].transformation, this.materials.skyscraper_material);
-                console.log("Building: " + this.skyscrapers_states[i].x + " " + this.skyscrapers_states[i].y + " " + this.skyscrapers_states[i].z + " ")
-                if (this.hasCollided(this.skyscrapers_states[i], this.player)) {
+                //console.log("Building: " + this.skyscrapers_states[i].x + " " + this.skyscrapers_states[i].y + " " + this.skyscrapers_states[i].z + " ")
+                if (this.hasCollided(this.skyscrapers_states[i], this.Player)) {
                     this.end_game = true;
                     console.log("ggs")
                 }
