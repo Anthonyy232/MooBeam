1737a2f3c8eba7057fec507a5d5a5efe96bf1964
andrew-w-u <andrewwu7@g.ucla.edu>
2024-03-14 04:30:57 -0700

    potential key remappings

--- mooBeam.js
+++ mooBeam.js
@@ -458,22 +458,22 @@ export class MooBeam extends Scene {
             this.camera_angle = this.stored_angle;
         });
 
-        this.key_triggered_button("Move forward", ["i"], this.move_forward, "#6E6460", () => {
+        this.key_triggered_button("Move forward", ["w"], this.move_forward, "#6E6460", () => {
             this.player.velocity.x = 0;
             this.player.velocity.z = 0;
             this.up = false;
         });
-        this.key_triggered_button("Move backward", ["k"], this.move_backward, "#6E6460", () => {
+        this.key_triggered_button("Move backward", ["s"], this.move_backward, "#6E6460", () => {
             this.player.velocity.x = 0;
             this.player.velocity.z = 0;
             this.down = false;
         });
-        this.key_triggered_button("Move left", ["j"], this.move_left, "#6E6460", () => {
+        this.key_triggered_button("Move left", ["a"], this.move_left, "#6E6460", () => {
             this.player.velocity.x = 0;
             this.player.velocity.z = 0;
             this.left = false;
         });
-        this.key_triggered_button("Move right", ["l"], this.move_right, "#6E6460", () => {
+        this.key_triggered_button("Move right", ["d"], this.move_right, "#6E6460", () => {
             this.player.velocity.x = 0;
             this.player.velocity.z = 0;
             this.right = false;
@@ -503,14 +503,35 @@ export class MooBeam extends Scene {
                 }, 1000);
             }
         });
-        this.key_triggered_button("Turn left", [","], this.turn_left, "#6E6460", () => {
+        this.key_triggered_button("Beam Cows", [" "], () => {
+            if (!this.beaming) {
+                this.show_beam = true;
+                this.check_cow_within_shadow()
+            }
+            if (this.show_beam) {
+                setTimeout(() => {
+                    this.show_beam = false;
+                    this.beaming = false;
+                }, 1000);
+            }
+        });
+        this.key_triggered_button("Turn left", ["ArrowLeft"], this.turn_left, "#6E6460", () => {
             this.camera_right = false;
             this.camera_left = false;
         });
-        this.key_triggered_button("Turn right", ["."], this.turn_right, "#6E6460", () => {
+        this.key_triggered_button("Turn right", ["ArrowRight"], this.turn_right, "#6E6460", () => {
             this.camera_right = false;
             this.camera_left = false;
         });
+        this.key_triggered_button("Turn left", ["q"], this.turn_left, "#6E6460", () => {
+            this.camera_right = false;
+            this.camera_left = false;
+        });
+        this.key_triggered_button("Turn right", ["e"], this.turn_right, "#6E6460", () => {
+            this.camera_right = false;
+            this.camera_left = false;
+        });
+
     }
 
     reset() {
